2b1a795cc712c3747523c6f4d1029b24eea00a0f
yasuhiroh8160 <uriochan@icloud.com>
2019-12-10 14:48:16 +0900

    group_function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,19 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
 
   def index
+    # @groups = Group.all
   end
 
   def edit
+  end
+
+  def update
+    if @group.update(group_params)
+      redirect_to root_path, notice: 'グループを更新しました'
+    else
+      render :edit
+    end
   end
 
   def new
@@ -23,5 +33,8 @@ class GroupsController < ApplicationController
   private
   def group_params
     params.require(:group).permit(:name, user_ids: [] )
+  end
+  def set_group
+    @group = Group.find(params[:id])
   end
 end
