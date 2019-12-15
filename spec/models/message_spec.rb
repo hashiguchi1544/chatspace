355500642ecf2365b2873be96940c80f2ad6490e
yasuhiroh8160 <uriochan@icloud.com>
2019-12-15 17:17:58 +0900

    message_function
    message_function

---
+++ spec/models/message_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Message, type: :model do
+  describe '#create' do
+    context 'can save' do
+　# この中にメッセージを保存できる場合のテストを記述
+    end
+
+    context 'can not save' do
+　# この中にメッセージを保存できない場合のテストを記述
+    end
+  end
+end
---
+++ spec/support/controller_macros.rb
@@ -0,0 +1,5 @@
+def login(user)
+  @request.env["devise.mapping"] = Devise.mappings[:user]
+  sign_in user
+end
+end
