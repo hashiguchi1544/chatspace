8d47ee1da41c4fa1078a498abf07d03211af27be
yasuhiroh8160 <uriochan@icloud.com>
2019-12-18 16:44:52 +0900

    fit cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 lock '3.11.2'
 
 # Capistranoのログの表示に利用する
-set :application, 'chat-space'
+set :application, 'chatspace'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:yasuhiroh8160/chatspace.git'
